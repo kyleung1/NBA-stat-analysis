50c3a92a01f0a0d344fad7172b71dbb0846040b8
kyleung1 <leungkyle2@gmail.com>
2024-03-08 13:23:09 -0800

    update supabase key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://wdhrryjkgypzxknnphol.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseKey);
 // uncomment supabase above to get real data, some reason supabase stopped
 // working but was working earlier. Probably a limit from the free tier.
