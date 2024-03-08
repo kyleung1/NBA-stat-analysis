6a6d60bff7604aa42f0503aed0366ed3c062db2b
kyleung1 <leungkyle2@gmail.com>
2024-03-08 13:11:42 -0800

    had to remove continuous integration

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://wdhrryjkgypzxknnphol.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY!;
-// export const supabase = createClient(supabaseUrl, supabaseKey);
-// uncomment above to get real data, some reason supabase stopped
+export const supabase = createClient(supabaseUrl, supabaseKey);
+// uncomment supabase above to get real data, some reason supabase stopped
 // working but was working earlier. Probably a limit from the free tier.
+
+// commenting out supabase will allow tests to pass (npx cypress run) (npm run test)
